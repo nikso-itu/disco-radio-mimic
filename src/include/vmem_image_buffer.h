a67fa87c1b22a522e02a523ef3818a2e104f2d70
nikso-itu <nikso@itu.dk>
2024-03-24 18:40:19 +0100

    Fixed copy-paste mistake in vmem_image_buffer.h

--- src/include/vmem_image_buffer.h
+++ src/include/vmem_image_buffer.h
@@ -4,4 +4,4 @@ extern vmem_t vmem_image_buffer;
 
 #define VMEM_IMAGE_BUFFER_HEAD 0x00 // 4 bytes for head
 #define VMEM_IMAGE_BUFFER_TAIL 0x04 // 4 bytes for tail
-#define VMEM_IMAGE_BUFFER_TAIL 0x08 // rest for image buffer
+#define VMEM_IMAGE_BUFFER_DATA 0x08 // rest for image buffer
