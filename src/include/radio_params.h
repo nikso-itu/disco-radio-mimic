2382314d0427cca5ae2febf256c5a74b1d2ddb3f
nikso-itu <nikso@itu.dk>
2024-03-24 18:41:27 +0100

    added ids for radio params (matches DIPP)

---
+++ src/include/radio_params.h
@@ -0,0 +1,11 @@
+#ifndef DIPP_PARAMIDS_H
+#define DIPP_PARAMIDS_H
+
+/* Parameter for running radio */
+#define PARAMID_RADIO_RUN 1
+
+/* Parameters for keeping track of image buffer */
+#define PARAMID_IMAGE_BUFFER_HEAD 60
+#define PARAMID_IMAGE_BUFFER_TAIL 61
+
+#endif
