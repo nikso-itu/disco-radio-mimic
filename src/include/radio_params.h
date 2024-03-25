fce9b6fbdbe1b9b569096ee8c18f1404122d2d21
nikso-itu <nikso@itu.dk>
2024-03-25 11:35:22 +0100

    header guards and setup param id
    Co-authored-by: Daniel Kjellberg <dakj@itu.com>
    Co-authored-by: Jeppe Lindhard <jepli@itu.com>

--- src/include/radio_params.h
+++ src/include/radio_params.h
@@ -1,8 +1,9 @@
-#ifndef DIPP_PARAMIDS_H
-#define DIPP_PARAMIDS_H
+#ifndef RADIO_PARAMS_H
+#define RADIO_PARAMS_H
 
 /* Parameter for running radio */
 #define PARAMID_RADIO_RUN 1
+#define PARAMID_RADIO_SETUP 2
 
 /* Parameters for keeping track of image buffer */
 #define PARAMID_IMAGE_BUFFER_HEAD 60
--- src/include/vmem_image_buffer.h
+++ src/include/vmem_image_buffer.h
@@ -1,7 +1,11 @@
-#pragma once
+#ifndef VMEM_IMAGE_BUFFER_H
+#define VMEM_IMAGE_BUFFER_H
+
 #include <vmem/vmem.h>
 extern vmem_t vmem_image_buffer;
 
 #define VMEM_IMAGE_BUFFER_HEAD 0x00 // 4 bytes for head
 #define VMEM_IMAGE_BUFFER_TAIL 0x04 // 4 bytes for tail
 #define VMEM_IMAGE_BUFFER_DATA 0x08 // rest for image buffer
+
+#endif
